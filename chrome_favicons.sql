ec7bd6cd3f9380604f83e68000abcfffde37aa69
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-08-08 13:36:11 +0300

    Add files via upload

--- chrome_favicons.sql
+++ chrome_favicons.sql
@@ -1,20 +1,19 @@
--- \AppData\Roaming\Mozilla\Firefox\Profiles\
-
-select 
-
-favicons.url,
-favicons.icon_type,
-favicons.id,
-
+select
+favicons.icon_type as 'Icon_type',
+icon_mapping.id as 'id',
+favicons.id as 'fid',
+favicon_bitmaps.id as 'bid',
+favicons.url as 'Favicon_url',
+icon_mapping.page_url as 'Page_url',
+case when favicon_bitmaps.last_updated <> 0 then datetime((favicon_bitmaps.last_updated/1000000)-11644473600,'unixepoch','localtime') else '' end as 'LastUpdated',
+case when favicon_bitmaps.last_requested <> 0 then datetime((favicon_bitmaps.last_requested/1000000)-11644473600,'unixepoch','localtime') else '' end as 'Lastrequested',
 favicon_bitmaps.height,
 favicon_bitmaps.icon_id,
 favicon_bitmaps.id,
 favicon_bitmaps.width,
-datetime((favicon_bitmaps.last_updated/1000000)-11644473600,'unixepoch','localtime') as 'LastUpdated',
-case when favicon_bitmaps.last_requested <> 0 then datetime((favicon_bitmaps.last_requested/1000000)-11644473600,'unixepoch','localtime') else '' end as 'Lastrequested',
-hex(favicon_bitmaps.image_data) as 'Image_data'
-
+hex(favicon_bitmaps.image_data) as 'Image_data(PNG)'
 
 from icon_mapping
-left join favicon_bitmaps on favicon_bitmaps.icon_id = icon_mapping.icon_id
-left join favicons on favicons.id = icon_mapping.icon_id
+join favicons on favicons.id = icon_mapping.icon_id
+left join favicon_bitmaps on favicon_bitmaps.id = favicons.id
+order by id desc
