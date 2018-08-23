79d13aeaafdd1f8ff4aa4782c493cd405a2657f6
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-08-23 06:28:44 +0300

    Update skype_cache_db.sql

--- skype_cache_db.sql
+++ skype_cache_db.sql
@@ -1,16 +1,14 @@
 select
-Assets.id,
+Assets.id as 'id',
 Assets.key as 'key',
-Assets.sub_key,
-
-assets.lock_count,
+Assets.sub_key as 'sub_key',
+assets.lock_count as 'lock_count',
 datetime(assets.access_time /10000000, 'unixepoch', 'localtime')  as 'AccessTime',
-assets.actual_size,
-assets.reserved_size,
-assets.type,
-assets.owner,
+assets.actual_size as 'actual_size',
+assets.reserved_size as 'reserved_size',
+assets.type as 'type',
+assets.owner as 'owner',
 hex(assets.serialized_data) as 'serial'
-
 from  Assets
-order by actual_size desc
+
 
