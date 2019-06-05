7cce87d1458ebaa6c3e8239890142ea322a08c9d
Costas K <11378310+kacos2000@users.noreply.github.com>
2019-06-05 15:37:52 +0300

    Android 9 contacts2.db calls query

---
+++ contacts2calls.sql
@@ -0,0 +1,18 @@
+Select 
+id,
+name,
+number,
+case type
+	when 1 then 'incoming' --incoming calls
+	when 2 then 'outgoing' --outgoing calls
+	when 3 then 'missed' --missed calls
+	when 4 then 'voicemail' --Call log type for voicemails
+	when 5 then 'rejected' --rejected by direct user action
+	when 6 then 'blocked' --calls blocked automatically
+	when 7 then 'answered externally' --call which was answered on another device
+	else type
+end as 'type',
+datetime(date/1000,'unixepoch','localtime') as 'date',
+duration
+from calls
+order by id desc
