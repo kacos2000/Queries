9882634f4b7002feb451e598f8fd279356d626d5
Costas K <11378310+kacos2000@users.noreply.github.com>
2019-06-25 11:54:22 +0300

    IOS

--- Accounts3_sqlite.sql
+++ Accounts3_sqlite.sql
@@ -1,5 +1,8 @@
+-- Home\Library\Accounts
+
 Select 
 datetime('2001-01-01', zaccount.zdate || ' seconds') as 'AccDate',
+datetime('2001-01-01', zaccount.ZLASTCREDENTIALRENEWALREJECTIONDATE || ' seconds') as 'RenewalRejection',
 ZACCOUNTTYPE.ZACCOUNTTYPEDESCRIPTION as 'AccTypeDescription',
 ZACCOUNTTYPE.ZIDENTIFIER as 'AccountIdentifier',
 zaccount.ZUSERNAME as 'Username',
@@ -18,15 +21,17 @@ case zaccount.ZSUPPORTSAUTHENTICATION
 	end as 'SupportsAuth',	
 zaccount.ZAUTHENTICATIONTYPE as 'AuthenticationType',
 ZACCOUNTTYPE.ZCREDENTIALTYPE as 'CredentialType',
+ZACCOUNTPROPERTY.ZKEY as 'key',
 case zaccount.ZVISIBLE 
 	when 0 then 'No'
 	when 1 then 'Yes' 
-	end as 'Visible',
+	end as 'AccVisible',
 zaccount.ZIDENTIFIER as 'Identifier',
 zaccount.ZOWNINGBUNDLEID as 'OwningBundleID'
 
 
 from zaccount
 join ZACCOUNTTYPE on ZACCOUNTTYPE.Z_PK = zaccount.'ZACCOUNTTYPE'
+join ZACCOUNTPROPERTY on ZACCOUNTPROPERTY.Z_PK = zaccount.Z_OPT
 
 order by AccDate desc
