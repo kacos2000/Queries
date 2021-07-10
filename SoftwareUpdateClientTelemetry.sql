49e441362523841e29df90076f34ee7083a64d27
Costas K <11378310+kacos2000@users.noreply.github.com>
2021-07-10 10:41:32 +0300

    Add files via upload

---
+++ SoftwareUpdateClientTelemetry.sql
@@ -0,0 +1,26 @@
+-- Diagnostic SoftwareUpdateClientTelemetry
+-- from C:\ProgramData\Microsoft\Diagnosis\EventTranscript\EventTranscript.db
+-- For more info visit https://github.com/rathbuna/EventTranscript.db-Research
+
+SELECT
+
+-- Timestamp from db field
+json_extract(events_persisted.payload,'$.time') as 'UTC TimeStamp',
+-- Timestamp from json payload
+datetime((timestamp - 116444736000000000)/10000000, 'unixepoch','localtime') as 'Local TimeStamp',
+producers.producer_id_text as Producer,
+json_extract(events_persisted.payload,'$.data.EventInstanceID') as 'EventInstanceID',
+json_extract(events_persisted.payload,'$.ext.utc.seq') as 'seq',
+json_extract(events_persisted.payload,'$.data.EventScenario') as 'Status',
+json_extract(events_persisted.payload,'$.data.CallerApplicationName') as 'CallerApplicationName',
+json_extract(events_persisted.payload,'$.data.NumberOfApplicableUpdates') as 'NrOfApplicableUpdates',
+json_extract(events_persisted.payload,'$.data.IntentPFNs') as 'IntentPFNs',
+coalesce(json_extract(events_persisted.payload,'$.data.HostName'),json_extract(events_persisted.payload,'$.data.ServiceUrl')) as 'HostName',
+json_extract(events_persisted.payload,'$.ext.utc.pgName') as 'pgName',
+json_extract(events_persisted.payload,'$.ext.utc.loggingBinary') as 'loggingBinary'
+
+FROM events_persisted
+join producers on producers.producer_id = events_persisted.producer_id
+join event_tags on events_persisted.full_event_name_hash = event_tags.full_event_name_hash
+where full_event_name like '%SoftwareUpdateClientTelemetry%'
+order by events_persisted.timestamp desc
