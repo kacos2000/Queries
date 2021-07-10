50481e4a443b61db56ff667d19a5dc3aaa1f4b28
Costas K <11378310+kacos2000@users.noreply.github.com>
2021-07-10 14:52:11 +0300

    Add files via upload
    Edge, Twitter app, Sticky notes, etc

--- Aria_WebBrowser.sql
+++ Aria_WebBrowser.sql
@@ -9,17 +9,22 @@ SELECT
 json_extract(events_persisted.payload,'$.time') as 'UTC TimeStamp',
 -- Timestamp from json payload
 datetime((timestamp - 116444736000000000)/10000000, 'unixepoch','localtime') as 'Local TimeStamp',
+
 -- Actions
-replace(replace(substr(distinct full_event_name,39),'Microsoft.',''),'WebBrowser.HistoryJournal.HJ_','') as 'event',
+coalesce(json_extract(events_persisted.payload,'$.data.ShortEventName'),replace(replace(substr(distinct full_event_name,39),'Microsoft.',''),'WebBrowser.HistoryJournal.HJ_','')) as 'event',
+json_extract(events_persisted.payload,'$.ext.utc.seq') as 'seq',
 json_extract(events_persisted.payload,'$.data.TabId') as 'TabId',
 coalesce(json_extract(events_persisted.payload,'$.data.PageTitle'),    json_extract(events_persisted.payload,'$.data.DOMAnchorHrefUrl')) as 'PageTitle/RefUrl',
 coalesce(json_extract(events_persisted.payload,'$.data.navigationUrl'),json_extract(events_persisted.payload,'$.data.DSPCurrentUrl'))    as 'Url',
 json_extract(events_persisted.payload,'$.data.actionName') as 'actionName',
-json_extract(events_persisted.payload,'$.ext.net.type') as 'type',
-json_extract(events_persisted.payload,'$.ext.net.cost') as 'cost'
+coalesce(json_extract(events_persisted.payload,'$.ext.net.type'),json_extract(events_persisted.payload,'$.data.ConnectionType')) as 'type',
+json_extract(events_persisted.payload,'$.ext.net.cost') as 'cost',
+json_extract(events_persisted.payload,'$.ext.app.name') as 'app',
+logging_binary_name
 
 from events_persisted 
-where events_persisted.full_event_name like ('Aria.%') and 
+where 
+events_persisted.full_event_name like ('Aria.%') and 
 events_persisted.full_event_name not like '%HeartBeat' and 
 events_persisted.full_event_name not like '%Timing%'   and 
 events_persisted.full_event_name not like '%EdgeUpdate%' and 
@@ -32,7 +37,6 @@ events_persisted.full_event_name not like '%Actor%' and
 events_persisted.full_event_name not like '%SessionIdCorrelation%' and 
 events_persisted.full_event_name not like '%ScopedCriticalTask%' and 
 events_persisted.full_event_name not like '%Assert%' and 
-events_persisted.full_event_name not like '%reportdata%' and 
 events_persisted.full_event_name not like '%BrowserInfo%' and 
 events_persisted.full_event_name not like '%diagnosticssync%' 
 order by events_persisted.timestamp desc
